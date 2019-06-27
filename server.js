11609db087cf4e11cf13eba24b4f94be54b57dca
ishtha511 <31902128+ishtha511@users.noreply.github.com>
2019-06-27 16:52:47 +0530

    Update server.js

--- server.js
+++ server.js
@@ -218,5 +218,39 @@ app.post('/createpost',isAuthenticated,function(req,res,err){
   res.send("success")
 })
 
+
+function loadMessages() {
+  // Create the query to load the last 12 messages and listen for new ones.
+  var query = firebase.firestore()
+                  .collection('messages')
+                  .orderBy('timestamp', 'desc')
+                  .limit(12);
+  
+  // Start listening to the query.
+  query.onSnapshot(function(snapshot) {
+    snapshot.docChanges().forEach(function(change) {
+      if (change.type === 'removed') {
+        deleteMessage(change.doc.id);
+      } else {
+        var message = change.doc.data();
+        displayMessage(change.doc.id, message.timestamp, message.name,
+                       message.text, message.profilePicUrl, message.imageUrl);
+      }
+    });
+  });
+}
+
+function saveMessage(messageText) {
+  // Add a new message entry to the Firebase database.
+  return firebase.firestore().collection('messages').add({
+    name: getUserName(),
+    text: messageText,
+    profilePicUrl: getProfilePicUrl(),
+    timestamp: firebase.firestore.FieldValue.serverTimestamp()
+  }).catch(function(error) {
+    console.error('Error writing new message to Firebase Database', error);
+  });
+}
+
 console.log("app running in port 2017 open 'localhost:2017/login' in your browser")
 app.listen(2017)
